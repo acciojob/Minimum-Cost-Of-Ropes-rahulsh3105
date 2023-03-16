184bbf626c0ccdb40a00fba52bc410b75ef20375
Rahul Sharma <rahulsh3105@gmail.com>
2023-03-16 13:41:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+  const input = document.getElementById("input").value;
+  const lengths = input.split(",").map((length) => parseInt(length));
+  const sortedLengths = lengths.sort((a, b) => a - b);
+  let cost = 0;
+  while (sortedLengths.length > 1) {
+    const sum = sortedLengths[0] + sortedLengths[1];
+    cost += sum;
+    sortedLengths.splice(0, 2, sum);
+    sortedLengths.sort((a, b) => a - b);
+  }
+  const resultDiv = document.getElementById("result");
+  resultDiv.innerText = cost;
+}
